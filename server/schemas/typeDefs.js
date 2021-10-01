f2f30a854cdb9f73a66cded72336750cbb99069f
Gina Im <eaim928@gmail.com>
2021-09-30 23:05:30 -0600

    started schema content

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,27 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Book {
+    _id: ID
+    author: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+  }
+
+
+
+  type Query {
+  
+  }
+`;
